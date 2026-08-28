0c3ba8a2f1d7da8fe857abf6a4a2e705ae6c0f7e
Filipe Vilas Bôas <59072469+filipesvb@users.noreply.github.com>
2026-08-28 14:24:51 +0000

    adicionados scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto clone do Tabnews, do Curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function App() {
-    const amorDaMinhaVida='Naiara';
-    
-    return <h1>Hello, {amorDaMinhaVida}. Tu é o amor da minha vida S2</h1>
+  const amorDaMinhaVida = "Naiara";
+
+  return <h1>Hello, {amorDaMinhaVida}. Tu é o amor da minha vida S2</h1>;
 }
 
 export default App;
