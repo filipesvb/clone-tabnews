29a725a25700f53ac2fc09af1cf0457048aa779c
Filipe Vilas Bôas <fvb.boas@gmail.com>
2026-09-02 18:36:17 -0300

    finalizado endpoint `/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,35 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ msg: "hello world" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResponse = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResponse.rows[0].server_version;
+
+  const databaseMaxConnectionsResponse = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResponse.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
+    values: [databaseName],
+  });
+  const databaseOpenConnectionsValue =
+    databaseOpenConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        open_connections: databaseOpenConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
